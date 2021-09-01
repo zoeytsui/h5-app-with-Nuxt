e165e775d502a2fdb3c7ed39d27caa62f0ce6484
zoey.xu <39@klnnn.com>
2021-09-01 16:26:30 +0800

    fixed connect ECONNREFUSED error

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,7 +89,7 @@ export default {
   },
 
   server: {
-    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : process.env.IP,
-    port: process.env.NODE_ENV === 'production' ? '8001' : process.env.PORT
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+    port: process.env.NODE_ENV === 'production' ? '8001' : '7070'
   }
 }
